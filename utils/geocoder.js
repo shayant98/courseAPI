0fe19def7b4999a556b001f3d123cccaca7ca065
Shayant Sital <shayant@gmail.com>
2019-10-26 00:31:50 -0300

    added node-geocoder added geocoder init file added geocoder key to env added geocoder fucntion to pre save hook

---
+++ utils/geocoder.js
@@ -0,0 +1,12 @@
+const nodeGeocoder = require("node-geocoder");
+
+const options = {
+  provider: process.env.GEOCODER_PROVIDER,
+  httpAdepter: "https",
+  apiKey: process.env.GEOCODER_API_KEY,
+  formatter: null
+};
+
+const geocoder = nodeGeocoder(options);
+
+module.exports = geocoder;
